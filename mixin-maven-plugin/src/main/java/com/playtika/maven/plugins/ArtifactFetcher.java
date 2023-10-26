61897229b4aa3347b9a34722b6a7d8bf45fe88c4
Igor Yova <iyova@playtika.com>
2023-10-26 14:29:49 +0300

    Cleanup project and fix version plugin dependency (#29)

--- mixin-maven-plugin/src/main/java/com/playtika/maven/plugins/ArtifactFetcher.java
+++ mixin-maven-plugin/src/main/java/com/playtika/maven/plugins/ArtifactFetcher.java
@@ -5,7 +5,7 @@ import org.apache.maven.artifact.resolver.ArtifactResolutionException;
 import org.apache.maven.execution.MavenSession;
 
 public interface ArtifactFetcher {
-    public Artifact createArtifact(String groupId, String artifactId, String type, String classifier, String version);
+    Artifact createArtifact(String groupId, String artifactId, String type, String classifier, String version);
 
-    public void resolve(Artifact artifact, MavenSession mavenSession) throws ArtifactResolutionException;
+    void resolve(Artifact artifact, MavenSession mavenSession) throws ArtifactResolutionException;
 }
