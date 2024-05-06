131b3ad83aac984fa1cf95c23ff3b1dff7828c55
Igor Yova <iyova@playtika.com>
2024-05-06 12:26:58 +0200

    Add mixin-maven-plugin version update and FakeMixinMojo for IDE support

---
+++ mixin-maven-plugin/src/main/java/com/playtika/maven/plugins/FakeMixinMojo.java
@@ -0,0 +1,27 @@
+package com.playtika.maven.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
+
+/**
+ * This mojo is a fake mojo just for having the mixins configuration available in IDEs such as intelliJ
+ *
+ * @author odavid
+ */
+@Mojo(name = "fake-mixin-mojo", threadSafe = true)
+public class FakeMixinMojo extends AbstractMojo {
+
+    @Parameter(property = "mixins")
+    private List<Mixin> mixins;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        //DO NOTHING...
+    }
+
+}
